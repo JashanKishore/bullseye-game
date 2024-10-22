d46c8788cc16ac6fd76aad468faa71616838ab0d
Jashan Kishore <jashan.kishore@gmail.com>
2024-10-22 12:52:40 +1100

    UI setup for game.

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -8,12 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var  currentValue: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
-    @IBAction func showAlert(_ sender: Any) {
+    @IBAction func showAlert() {
         let alert = UIAlertController(
             title: "Hello, World",
             message: "This is my first app!",
@@ -26,6 +28,11 @@ class ViewController: UIViewController {
 
           alert.addAction(action)
           present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        //print("The value of slider is now: \(slider.value)")
+        currentValue = lroundf(slider.value)
     }
     
 }
